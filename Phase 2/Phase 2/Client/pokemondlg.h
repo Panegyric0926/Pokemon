b28d398a44f25906cb49f9b75806bfb5fc7d2906
洣祺 李 <18801330288@163.com>
2020-08-20 15:53:44 +0800

    client: prototype

---
+++ Phase 2/Phase 2/Client/pokemondlg.h
@@ -0,0 +1,32 @@
+#ifndef POKEMONDLG_H
+#define POKEMONDLG_H
+
+#include <QDialog>
+#include <QTableWidget>
+#include <QLabel>
+
+namespace Ui
+{
+	class PokemonDlg;
+}
+
+// dialog to show pokemon details
+class PokemonDlg : public QDialog
+{
+	Q_OBJECT
+
+public:
+	explicit PokemonDlg(const QString& detail, bool myPokemonTable = true, QWidget* parent = 0);
+	~PokemonDlg();
+
+	QTableWidget* table; // attribute table
+	QLabel* lbImg;			 // race image
+
+signals:
+	void pokemonChangeName(const QString& pokemonID, const QString& newName);
+
+private:
+	Ui::PokemonDlg* ui;
+};
+
+#endif // POKEMONDLG_H
